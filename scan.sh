d991935c6f535589143c15b97f563d97ed275f9d
Miroslav Suchý <msuchy@redhat.com>
2024-06-20 16:34:11 +0200

    initial work

---
+++ scan.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+set -e
+
+PACKAGE=$1
+ARCHIVE=$2
+BUILDDIR=$(mktemp -d --suffix license-scan)
+NEWARCHIVE=$(mktemp -d --suffix license-scan)
+
+cd /tmp
+fedpkg clone -a "$PACKAGE"
+cd "$PACKAGE"
+fedpkg prep --builddir "$BUILDDIR"
+
+cd "$BUILDDIR"
+~/.local/bin/scancode --license --license-references -n6 --strip-root --html /tmp/scan.html --json-pp /tmp/scan.json .
+
+cd "$NEWARCHIVE"
+tar axvf "$2"
+~/.local/bin/scancode --license --license-references -n6 --strip-root --html /tmp/scan2.html --json-pp /tmp/scan2.json .
