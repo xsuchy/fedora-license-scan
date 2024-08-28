fa3615d7f2e8526b35e10a1e8ed7323dbb4adddd
Miroslav Suchý <msuchy@redhat.com>
2024-08-28 21:51:18 +0200

    use real num cpu

--- scan.sh
+++ scan.sh
@@ -7,16 +7,18 @@ ARCHIVE=$2
 BUILDDIR=$(mktemp -d --suffix license-scan)
 NEWARCHIVE=$(mktemp -d --suffix license-scan)
 
+NUMCPU=$(grep -c ^processor /proc/cpuinfo)
+
 cd /tmp
 fedpkg clone -a "$PACKAGE"
 cd "$PACKAGE"
 fedpkg prep --builddir "$BUILDDIR"
 
 cd "$BUILDDIR"
-~/.local/bin/scancode --license --license-references -n6 --strip-root --html /tmp/scan.html --json-pp /tmp/scan.json .
+~/.local/bin/scancode --license --license-references -n$NUMCPU --strip-root --html /tmp/scan.html --json-pp /tmp/scan.json .
 
 cd "$NEWARCHIVE"
 tar axvf "$2"
-~/.local/bin/scancode --license --license-references -n6 --strip-root --html /tmp/scan2.html --json-pp /tmp/scan2.json .
+~/.local/bin/scancode --license --license-references -n$NUMCPU --strip-root --html /tmp/scan2.html --json-pp /tmp/scan2.json .
 
 python3 compare-scan-json.py
